e2de84507eeff4f360b27046c4df72e36dc4d306
LCT429 <103705907+LCT429@users.noreply.github.com>
2022-05-10 11:04:34 +0800

    Update keymap_common.h

--- keyboard/newhope64/keymap_common.h
+++ keyboard/newhope64/keymap_common.h
@@ -32,46 +32,18 @@ extern const action_t fn_actions[];
 /* GH60 keymap definition macro
  * K2C, K31 and  K3C are extra keys for ISO
  */
-#define KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A, K4B  \
+#define ACTIONMAP_ENC( \
+	K00, K01, K02, K03,    \
+	     K11, K12, K13,    \
+	K20, K21, K22,         \
+	K30, K31, K32,    \
+	K40,           K43,  \
+	R0P, R0N, K44\
 ) { \
-	{ K00,    K01,    K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K0A,    K0B,    K0C,    K0D }, \
-	{ K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K1A,    K1B,    K1C,    K1D }, \
-	{ K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K2A,    K2B,    K2C,    KC_NO }, \
-	{ K30,    K31,    K32,    K33,    K34,    K35,    K36,    K37,    K38,    K39,    K3A,    K3B,    K3C,    KC_NO }, \
-	{ K40,    K41,    K42,    K43,    KC_NO,  KC_NO,  K46,    K47,    K48,    K49,    K4A,    K4B,    KC_NO,  KC_NO }  \
+	{ AC_##K00, AC_##K01, AC_##K02, AC_##K03,   AC_NO }, \
+	{ AC_NO,    AC_##K11, AC_##K12, AC_##K13,   AC_NO }, \
+	{ AC_##K20, AC_##K21, AC_##K22, AC_NO,      AC_NO }, \
+	{ AC_##K30, AC_##K31, AC_##K32, AC_NO,      AC_NO }, \
+	{ AC_##K40, AC_##R0P, AC_##R0N, AC_##K43,   AC_##K44   }, \
 }
 
-/* ANSI variant. No extra keys for ISO */
-#define KEYMAP_ANSI( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A, K4B  \
-) KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A, K4B  \
-)
-
-
-#define KEYMAP_HHKB( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A, K4B  \
-) KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A, K4B  \
-)
